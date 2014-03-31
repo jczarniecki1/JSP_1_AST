f4e2993fd331d3e13ea2b964d94696a5e5445b17
mtomkowicz <gocha71@gmail.com>
2014-03-31 23:17:28 +0200

    Dodałam CommaExpressionTest

---
+++ src/edu/pjwstk/tests/expression/binary/CommaExpressionTest.java
@@ -0,0 +1,29 @@
+package edu.pjwstk.tests.expression.binary;
+
+import edu.pjwstk.demo.expression.Expression;
+import edu.pjwstk.demo.expression.binary.CommaExpression;
+import edu.pjwstk.demo.expression.binary.PlusExpression;
+import edu.pjwstk.demo.expression.terminal.IntegerExpression;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Małgorzata on 31.03.14.
+ */
+public class CommaExpressionTest extends AbstractBinaryExpressionTest {
+    @Test
+    public void shoudGiveStructOf2Integer() throws Exception {
+        Expression e = new CommaExpression(
+                new IntegerExpression(1),
+                new PlusExpression(
+                    new IntegerExpression(2),
+                    new IntegerExpression(1)
+                )
+        );
+        e.accept(visitor);
+        boolean result = qres.pop().toString().equals("struct(0=1,1=3)");
+
+        assertEquals(true,result);
+    }
+}
